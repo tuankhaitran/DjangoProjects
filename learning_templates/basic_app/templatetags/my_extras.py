ffb312ebf9bca13cf2423052604a72a9c7fddcd6
tuankhaitran <trankhaituan112@gmail.com>
2020-12-26 13:31:31 -0800

    Add learning-template

---
+++ learning_templates/basic_app/templatetags/my_extras.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='cut')
+def cut(value, arg):
+    """
+    This cut out all values of arg from the string
+    """
+    return value.replace(arg,'')
+
---
+++ learning_templates/basic_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.conf.urls import url
+from basic_app import views
+
+# TEMPLATE TAGGING
+
+app_name='basic_app'
+
+
+urlpatterns = [
+    url(r'^relative/$',views.relative,name='relative'),
+    url(r'^other/$',views.other,name='other')
+]
